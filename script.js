e8022c75f9c2cd837c55c9d8384251cee6b7985e
Mike Lyndon Bigcas <mcpemike04@gmail.com>
2022-09-30 19:06:56 +0800

    add files

---
+++ script.js
@@ -0,0 +1,54 @@
+let previewProduct = document.querySelector('.product--overlay10');
+let previewBox = previewProduct.querySelectorAll('.overlay10');
+
+document.querySelectorAll('.product-container10 .product').forEach(product =>{
+  product.onclick = () =>{
+    previewProduct.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-circle-xmark').onclick = () =>{
+    close.classList.remove('active');
+    previewProduct.style.display = 'none';
+  };
+});
+
+/* REVIEW */
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
